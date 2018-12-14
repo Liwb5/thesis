fc6a6d8fb61332f8e30eb11b3f2bbc7da5c3f203
big <liwb5@foxmail.com>
2018-12-14 15:57:42 +0800

    successful get config from json file

---
+++ utils/config.py
@@ -0,0 +1,38 @@
+import yaml
+import json
+import os
+import sys
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def get_config_from_yaml(yaml_file):
+    """
+    Get the config hyperparameters from yaml file.
+    Using AttrDict comtainer to place these hyperparameters.
+    """
+    return AttrDict(yaml.load(open(yaml_file, 'r')))
+
+
+def get_config_from_json(json_file):
+    """
+    Get the config from a json file
+    :param json_file:
+    :return: config(dictionary) 
+    """
+    # parse the configurations from the config json file provided
+    with open(json_file, 'r') as config_file:
+        config = json.load(config_file)
+
+    return config
+
+#  def replace_config(new_config, old_config):
+#      replaceable_params = ['epochs', 'n_gpu', 'batch_size', 'print_every', 'report_every']
+
+def process_config(json_file):
+    config, _ = get_config_from_json(json_file)
+    config.summary_dir = os.path.join("../experiments", config.exp_name, "summary/")
+    config.checkpoint_dir = os.path.join("../experiments", config.exp_name, "checkpoint/")
+    return config
