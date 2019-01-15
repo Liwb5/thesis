ec93766d4228aaaafd459a124af6d4235a0cfdb9
big <liwb5@foxmail.com>
2019-01-15 23:06:25 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-python main.py -c ./configs/debug_config.yaml -d 0
+# python main.py -c ./configs/debug_config.yaml -d 0
+python main.py -c ./configs/config.yaml -d 0
 # python main.py -c ./configs/config_ae.json -d 0
 # python main.py -c ./configs/config.json #-d 0
