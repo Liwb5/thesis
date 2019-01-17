3d65a845a4eee3fd76b1fbd546382d51b8a64405
big <liwb5@foxmail.com>
2019-01-17 21:39:49 +0800

    add some function to pretty print result when testing

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 from pprint import pprint, pformat
 
 import torch
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import models
 import models.loss as module_loss
@@ -32,6 +33,7 @@ class Test():
         self.test_data_loader = test_data_loader
         self.metrics = metrics
         self.vocab = vocab
+        self.device = config['device']
 
     def _eval_metrics(self, docs, pointers, reference):
         #  hypothesis = self.vocab.features_to_tokens(predicts.numpy().tolist())
@@ -50,7 +52,7 @@ class Test():
         STATS = ["f", "p", "r"]   
         final_test_metrics = {m:{s: 0.0 for s in STATS} for m in METRICS}
         with torch.no_grad():
-            for step, dataset in enumerate(self.test_data_loader):
+            for step, dataset in enumerate(tqdm(self.test_data_loader)):
                 docs_features, doc_lens, docs_tokens, \
                     sum_features, sum_target, sum_word_lens, sum_ref, \
                     labels, label_lens = self.vocab.data_to_features(dataset)
@@ -71,6 +73,7 @@ class Test():
                 att_probs, selected_probes, pointers, _ = self.model(docs_features, doc_lens, sum_features, sum_word_lens, labels, label_lens, tfr=0)
 
                 test_metrics.append(self._eval_metrics(dataset['doc'], pointers, sum_ref))
+                logging.info(pformat(pointers.data.cpu().numpy()))
 
             for i in range(len(test_metrics)):
                 for m in METRICS:
@@ -79,11 +82,25 @@ class Test():
             final_test_metrics = {m: {s: final_test_metrics[m][s] / len(test_metrics) for s in STATS}
                                 for m in METRICS}
 
-        return {'task_name': self.config['task_name'], 
-            'test_metrics': final_test_metrics}
+        return final_test_metrics
+
+
+def pretty_print(dictionary):
+    for rouge_n, scores in sorted(dictionary.items(), key=lambda p:p[0]):
+        logging.info(rouge_n)
+        for k, v in sorted(scores.items(), key=lambda p:p[0]):
+            logging.info([k, v])
 
 def test(config, resume):
-    test_data = Dataset(config['data_loader']['test_data'], data_quota = -1)
+    log_format='%(asctime)s-%(levelname)s-%(name)s: %(message)s'
+    logging.basicConfig(filename = ''.join((config['trainer']['args']['output_dir'], 'result.txt')),
+                        filemode = 'a',
+                        level = getattr(logging, config['log_level'].upper()),
+                        format = log_format)
+
+    logging.info(['resume path: ', resume])
+
+    test_data = Dataset(config['data_loader']['test_data'], data_quota = 100)
     test_data_loader = DataLoader(dataset = test_data,
                             batch_size = config['data_loader']['batch_size'])
     logging.info('using %d examples to test. ' % len(test_data))
@@ -91,7 +108,7 @@ def test(config, resume):
     vocab = Vocab(**config['vocabulary'], embed=None)
     # build model architecture
     model = getattr(models, config['model']['type'])(config['model']['args'], device=config['device'])
-    logging.info(['model infomation: ', model])
+    #  logging.info(['model infomation: ', model])
 
     # get function handles of loss and metrics
     weights = torch.ones(config['model']['args']['vocab_size'])
@@ -101,13 +118,22 @@ def test(config, resume):
     metrics = getattr(module_metrics, config['metrics']) 
 
     checkpoint = torch.load(resume)
+
     model.load_state_dict(checkpoint['state_dict'])
     if config['device'] is not None:
         model = model.cuda()
 
     t = Test(model, loss, config, test_data_loader, metrics, vocab)
     result = t.test()
-    logging.info(result)
+
+    # print valid metrics result
+    for epoch, metrics_result in sorted(checkpoint['logger'].entries.items(), key=lambda p:p[0]):
+        logging.info('%d epoch:'%epoch)
+        pretty_print(metrics_result['val_metrics'])
+
+    # print testing result
+    logging.info('testing result: ')
+    pretty_print(result)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='thesis_test')
