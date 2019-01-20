884036031728d2bbd6c1543911f422df0c9c9d98
big <liwb5@foxmail.com>
2019-01-20 17:06:35 +0800

    update run.sh, do not use -d parameter

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-# python main.py -c ./configs/debug_config.yaml -d 0
-python main.py -c ./configs/config.yaml -d 0
-# python main.py -c ./configs/config_ae.json -d 0
-# python main.py -c ./configs/config.json #-d 0
+# python main.py -c ./configs/debug_config.yaml
+# python main.py -c ./configs/test_config.yaml
+python main.py -c ./configs/config.yaml
