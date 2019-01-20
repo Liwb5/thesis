14722a2fa4124055b1214f0b08c7384b53b2d44d
big <liwb5@foxmail.com>
2019-01-20 16:53:53 +0800

    using tanh and explorate_rate in Attention to contrain the scope of attention result(alpha) so that the model can explorate in sample index of sentences

--- models/attention.py
+++ models/attention.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 import logging
+import random
+import math
 from pprint import pprint, pformat
 import torch
 import torch.nn as nn
@@ -11,8 +13,8 @@ class Attention(nn.Module):
     Attention model for Pointer-Net
     """
 
-    def __init__(self, input_dim,
-                 hidden_dim):
+    def __init__(self, input_dim, hidden_dim,
+                use_tanh=False, explorate_rate=4, device=None):
         """
         Initiate Attention
         :param int input_dim: Input's diamention
@@ -24,16 +26,19 @@ class Attention(nn.Module):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
-        self.input_linear = nn.Linear(input_dim, hidden_dim)
-        self.context_linear = nn.Conv1d(input_dim, hidden_dim, 1, 1)
+        self.input_linear = nn.Linear(input_dim, hidden_dim)  # project query
+        self.context_linear = nn.Conv1d(input_dim, hidden_dim, 1, 1)   # project context
         self.V = Parameter(torch.FloatTensor(hidden_dim), requires_grad=True)
         self._inf = Parameter(torch.FloatTensor([float('-inf')]), requires_grad=False)
         self.tanh = nn.Tanh()
         self.softmax= nn.Softmax(dim=1)
+        self.use_tanh = use_tanh
+        self.device = device
+        self.explorate_rate = explorate_rate
         #  self.log_softmax= F.log_softmax(dim=1)
 
         # Initialize vector V
-        nn.init.uniform_(self.V, -1, 1)
+        nn.init.uniform_(self.V, -1 / math.sqrt(hidden_dim), 1 / math.sqrt(hidden_dim))
 
     def forward(self, input,
                 context,
@@ -46,6 +51,7 @@ class Attention(nn.Module):
         :return: tuple of - (Attentioned hidden state, Alphas)
         """
 
+        verbosity = True if random.random() < -0.005 else False # for debug
         # (batch, hidden_dim, seq_len)
         # 为了让decoder是hidden size与 context的维度一致
         inp = self.input_linear(input).unsqueeze(2).expand(-1, -1, context.size(1))
@@ -59,9 +65,22 @@ class Attention(nn.Module):
 
         # (batch, seq_len)
         att = torch.bmm(V, self.tanh(inp + ctx)).squeeze(1)
+
+        if verbosity:
+            logging.info(['origin attention prob:', att[0]])
+        # constrain the scope of att prob, so that after softmax, sample index can do exploration 
+        if self.use_tanh:
+            att = self.explorate_rate * self.tanh(att)
+
+        if verbosity:
+            logging.info(['after tanh attention prob:', att[0]])
+
         if len(att[mask]) > 0:
             att[mask] = self.inf[mask]
+
         alpha = self.softmax(att)
+        if verbosity:
+            logging.info(['alpha:', alpha[0]])
         #  alpha = F.log_softmax(att, dim=1)
 
         hidden_state = torch.bmm(ctx, alpha.unsqueeze(2)).squeeze(2)
@@ -71,3 +90,4 @@ class Attention(nn.Module):
     def init_inf(self, mask_size):
         self.inf = self._inf.unsqueeze(1).expand(*mask_size)
         #  logging.debug(['attention init_inf(expected B, max_doc_len[3]): ', pformat(self.inf.data.cpu().numpy())])
+
