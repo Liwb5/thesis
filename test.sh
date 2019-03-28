eb7f774fb41f337eb21ec75fd3047eb8d4dfc5be
Webbley <liwb5@foxmail.com>
2019-03-28 14:11:20 +0800

    update test.sh so that we do not need to source activate root

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 task_name=$1
 epoch=$2
 max_selected=$3
-limit=$4
+device=$4
+limit=$5
 
 fun() {
-    python test.py -r ./checkpoints/$task_name/checkpoint-model_RL_AE-epoch$epoch.pth -m $max_selected &
+    python test.py -d $device -r ./checkpoints/$task_name/checkpoint-model_RL_AE-epoch$epoch.pth -m $max_selected &
     wait
 
     if [ $? -ne 0 ]; then
@@ -14,17 +15,18 @@ fun() {
         echo "[INFO] successful to run test.py."
     fi
 
-    source activate root
-    wait
+    # source activate root
+    # wait
 
     if [ "$limit" = "b" ]; then
         echo "limit"
         python my_rouge.py -b_limit -dir ./outputs/$task_name/ -epoch $epoch > ./outputs/$task_name/rouge155_result_${epoch}_${max_selected}_275.txt 2>&1 &
+        wait
     else
         echo "no limit"
         python my_rouge.py -dir ./outputs/$task_name/ -epoch $epoch > ./outputs/$task_name/rouge155_result_${epoch}_${max_selected}.txt 2>&1 &
+        wait
     fi
-    wait
 
     if [ $? -ne 0 ]; then
         echo "[ERROR] fail to run my_rouge.py."
