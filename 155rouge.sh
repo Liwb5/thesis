cc34dba13737f09fc65de119ecf50d8758325f41
big <liwb5@foxmail.com>
2019-03-29 13:56:30 +0800

    fix bug in 155rouge.sh file

--- 155rouge.sh
+++ 155rouge.sh
@@ -9,6 +9,6 @@ if [ "$limit" = "b" ]; then
     python my_rouge.py -b_limit -dir ./outputs/$task_name/ -epoch $epoch > ./outputs/$task_name/rouge155_result_${epoch}_${max_selected}_275.txt 2>&1 &
 else
     echo "no limit"
-    python my_rouge.py -dir ./outputs/$task_name/ -epoch $epoch > ./outputs/$task_name/rouge155_result_$epoch_${max_selected}.txt 2>&1 &
+    python my_rouge.py -dir ./outputs/$task_name/ -epoch $epoch > ./outputs/$task_name/rouge155_result_${epoch}_${max_selected}.txt 2>&1 &
 fi
 # source activate pytorch
